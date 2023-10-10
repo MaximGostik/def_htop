82040adc9f412a3d2282760775901d54cd141f92
Maxim Gostik <maximkadrivellnvg@gmail.com>
2023-10-10 22:13:28 +0300

    Add decorators

--- def_htop.py
+++ def_htop.py
@@ -1,10 +1,19 @@
-import psutil
-import pandas as pd
-pd.set_option('display.max_rows', None)
-from datetime import timedelta
+import psutil, json, datetime
 from time import time
 
+def write_cpu_info(func):
+    file_name = "CPU-Info.json"
 
+    def worker():
+        res = func()
+        with open(file_name, "w") as file:
+            file.write(json.dumps(res, indent=4))
+        return res
+
+    return worker
+
+
+@write_cpu_info
 def get_cpu_percent(): #возвращает сведения о загруженности ядер
 
     data = psutil.cpu_percent(interval=1, percpu=True)
@@ -23,7 +32,17 @@ def show_cpu_percent(): #выводит сведения о загруженно
         counter = info[i] // 5
         print(i, '[' + '|' * round(counter) + '.' * (20 - round(counter)), info[i], '%]', end = '\n')
 
+def write_mem_info(func):
 
+    file_name = "Mem-Info.json"
+    def worker():
+        res = func()
+        with open(file_name, "w") as file:
+            file.write(json.dumps(res, indent=4))
+        return res
+    return worker
+
+@write_mem_info
 def get_mem_info():#возвращает сведения об используемой оперативной памяти
 
     data = psutil.virtual_memory()
@@ -45,12 +64,40 @@ def show_mem_info():#выводит сведения об используемо
     mem_total = round(data['total'] / 1024**3, 2)
     print('\nMem[' + '|' * counters + '.' * (20 - counters), mem_used, '/', mem_total, 'Gb]')
 
+def write_load_average(func):
+
+    file_name = "Load-Average-Info.json"
+    def worker():
+        res = func()
+        with open(file_name, "w") as file:
+            file.write(json.dumps(res, indent=4))
+        return res
+
+    return worker
+@write_load_average
+def get_load_average():#выводит сведения о средней загруженности
+
+    return [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
+
 
 def show_load_average():#выводит сведения о средней загруженности
 
-    print('Load average:', *[x / psutil.cpu_count() * 100 for x in psutil.getloadavg()])
+    print('Load average:', *get_load_average())
 
+def write_swap_memory(func):
+    file_name = "Swap.memory_Info.json"
 
+    def worker():
+        res = func()
+        with open(file_name, "w") as file:
+            file.write(json.dumps(res, indent=4))
+        return res
+
+    return worker
+@write_swap_memory
+def get_swap_memory():
+
+    return psutil.swap_memory()
 def show_swap_memory():
 
     info = psutil.swap_memory()
@@ -59,36 +106,62 @@ def show_swap_memory():
     used = info[1] // 1024 ** 2
     print('Swp[' + '|' * round(used / total / 20) + '.' * (20 - round(used / total / 20)), used, 'Мб /', total, 'Мб]')
 
+def write_up_time(func):
+
+    file_name = "Up_Time_Info.json"
+
+    def worker():
+
+        res = func()
+        with open(file_name, "w") as file:
+            file.write(json.dumps(res))
+        return res
+
+    return worker
+
+@write_up_time
+def get_up_time():
+
+    dt = datetime.timedelta(seconds=time() - psutil.boot_time())
+    dt = str(dt)
+    return dt
 
 def show_up_time():
 
-    print('Uptime:', timedelta(seconds=time()-psutil.boot_time()))
+    print('Uptime:', get_up_time())
 
 
-def show_pids():
+def write_pids(func):
 
-    info, PID, name, status = [], [], [], []
+    file_name = "Pids.json"
 
-    for i in psutil.process_iter(['pid', 'name', 'username']):
-        i = str(i)
-        info.append(i[15:-1].split(','))
+    def worker():
+        res = func()
+        with open(file_name, "w") as file:
+            file.write(json.dumps(res, indent=4))
+        return res
 
-    for j in info:
-        PID.append(j[0][4:])
-        name.append(j[1][7:-1])
-        status.append(j[2][9:-1])
+    return worker
 
-    new_data = pd.DataFrame({
-        'PID':PID,
-        'NAME':name,
-        'STATUS':status
-    })
+@write_pids
+def get_pids():
+    info = []
+
+    for i in psutil.process_iter(['pid', 'name', 'status']):
+        info.append(i.info)
+
+    return info
+
+def show_pids(info):
 
     print('Tasks:', len(psutil.pids()), '\n')
-    print(new_data)
+    format_pid = "{pid:<7} {name:<28} {status:<20}"
+    print("{:<7} {:<28} {:<20}".format("PID", "Name", "Status"))
+    for p in info:
+        print(format_pid.format(**p))
 
 
 def htop():
-    return show_cpu_percent(), show_mem_info(), show_swap_memory(), show_load_average(), show_up_time(), show_pids()
+    return show_cpu_percent(), show_mem_info(), show_swap_memory(), show_load_average(), show_up_time(), show_pids(get_pids())
 
 htop()
